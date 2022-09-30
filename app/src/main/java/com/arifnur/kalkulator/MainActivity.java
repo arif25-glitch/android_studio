f535fbdf3926c5a982a66ff8be8009c629f6b348
Arif Nur <nnarif37@gmail.com>
2022-09-30 19:00:20 +0700

    new button added

--- app/src/main/java/com/arifnur/kalkulator/MainActivity.java
+++ app/src/main/java/com/arifnur/kalkulator/MainActivity.java
@@ -3,6 +3,10 @@ package com.arifnur.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button count_btn = (Button) findViewById(R.id.button);
+
+        count_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView editText = (TextView) findViewById(R.id.textView);
+                editText.setText("1 + 1 = 2");
+                editText.setText(Integer.toString(12));
+            }
+        });
     }
 }
